0cfff888972860b83ffb3994153b0bf7e2b73afb
Amal Chaudhuri <amalc@pobox.com>
2012-03-21 09:11:15 -0400

    1. Intial rails release.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyPhotogallery::Application.config.secret_token = '5668e57915a3bdba56dafd052ddaf60ad6f1f55caf1ca98a032b38ee8143eb1088f8364919314d2d0515627e2ad6b77f8b6168f01b0f6a4c1d5c0ad529918244'
