174a0052872eda0c53193b1eb9d6f2befcf566c3
jdservice <jdservice@igspartners.com>
2012-03-28 18:02:33 +0100

    Mine.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Group do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:all) do
+    @list_of_groups = [
+      ['Group 1', 'Group Description 1'],
+      ['Group 2', 'Group Description 2'],
+      ['Group 3', 'Group Description 3'],
+      ['Group 4', 'Group Description 4'],
+      ['Group 5', 'Group Description 5']
+      ]
+  end
+  
+  it "creates groups given a list of images" do
+    @list_of_groups.each do |g|
+      pp g
+      #Group.create!(
+      #  :name=> g[0],
+      #  :description=> g[1]
+      )
+    end
+  end
 end
