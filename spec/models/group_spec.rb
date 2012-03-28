f55c537a5e27d2fc6cd06a5657c757fa65945a35
Tim Davis <timothy.davis@virgin.net>
2012-03-28 21:52:32 +0100

    1. Add uniqueness in group.rb 2. Simple rspec checks in group file

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,9 +4,9 @@ describe Group do
   
   before(:all) do
     @list_of_groups = [
-      ['Group 1', 'Group Description 1'],
+      ['Groupe 1', 'Group Description 1'],
       ['Group 2', 'Group Description 2'],
-      ['Group 3', 'Group Description 3'],
+      ['Group 2', 'Group Description 3'],
       ['Group 4', 'Group Description 4'],
       ['Group 5', 'Group Description 5']
       ]
@@ -18,8 +18,11 @@ describe Group do
         :name => g[0],
         :description => g[1])
     end
+    
+    # simple test examples
     Group.all.count.should eq(5)
-    puts Group.find_by_name('Group 1').name
+    Group.find_by_name(@list_of_groups[0][0]).name.should eq(@list_of_groups[0][0])
+    Group.find_by_name(@list_of_groups[0][0]).name.should_not eq(@list_of_groups[1][0])    
     
   end
   
