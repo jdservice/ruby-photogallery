a2eacdc6c2e913372ec4824269bb8b530af044c5
Tim Davis <timothy.davis@virgin.net>
2012-03-28 18:04:47 +0100

    1. playing around

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -6,10 +6,31 @@ end
 
 describe Group do
   
+  before(:all) db
+    @list_of_groups = [
+      ['group1','group description'],  
+      ['group2','group description'],  
+      ['group3','group description'],  
+      ['group4','group description'],  
+      ['group5','group description']  
+      ]
+  end
+  
+  it "create groups given a list of groups" do
+    @list_of_groups.each do |g|
+      pp g
+      # Group.create!(
+      #  :name => g[0],
+      #  :description => g[1]
+      )
+    end
+  end
+  
   it "should create a group with a name of group" do
     group = Group.create!(
       :name => 'test group'
     )
     group.name.should eq('test group')
   end
+
 end
