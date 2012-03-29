16b1ff9d6a680d41ce6e458088c41a998ef25571
Amal Chaudhuri <amalc@pobox.com>
2012-03-28 21:17:54 -0400

    1. Updated tests.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ActiveRecord::Base
   
   has_many :images
-  
+  validates_uniqueness_of :name, :on => :create, :message => "muust be unique"
+
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,23 +4,18 @@ describe Group do
   
   before(:all) do
     @list_of_groups = [
-      ['Group 1', 'Group Description 1'],
+      ['Groupe 1', 'Group Description 1'],
       ['Group 2', 'Group Description 2'],
-      ['Group 3', 'Group Description 3'],
+      ['Groupe 1', 'Group Description 3'],
       ['Group 4', 'Group Description 4'],
       ['Group 5', 'Group Description 5']
       ]
   end
   
   it "creates groups given a list of groups" do
-    @list_of_groups.each do |g|
-      Group.create!(
-        :name => g[0],
-        :description => g[1])
-    end
-    Group.all.count.should eq(5)
-    puts Group.find_by_name('Group 1').name
-    
+    Group.create!(:name => @list_of_groups[0][0], :description => @list_of_groups[0][1])
+    expect { Group.create!(:name => @list_of_groups[0][0], 
+      :description => @list_of_groups[0][1]) }.to raise_error('Validation failed: Name must be unique') 
   end
   
 end
