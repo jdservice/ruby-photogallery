21b77dee1056a741915cc69bd24d23fa66f76879
Amal Chaudhuri <amalc@pobox.com>
2012-04-11 13:41:49 -0400

    1. Validating uniqueness of tag.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -12,4 +12,6 @@ class Tag < ActiveRecord::Base
 
   has_and_belongs_to_many :images
 
+  validates_uniqueness_of :name
+
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -16,6 +16,8 @@ require 'spec_helper'
 
 describe Image do
 
+  tags = %w(tag-1 tag-2 tag-3 tag-4 tag-5)
+
   it "should create an image given a file name" do
     image = Image.create!(
       :file_name => 'xyzz.jpg'
@@ -24,7 +26,7 @@ describe Image do
   end
 
   it "should create a tag given an image" do
-
+    pp tags
   end
 
 end
