c10c5e6fe31e02d959974c0e3a30f107b7917f76
Tim Davis <timothy.davis@virgin.net>
2012-04-17 19:06:54 +0100

    1. Removal of entities table (sorry, object) in favour of enumerated types 2. Setup many-to-many (sorry has and belongs to many) relationship to images 3. Very simple test in images_spec.rb to create 5 test enumerated types
    Note: Need to rake db:drop, create, generate and test:prepare

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,5 @@ class Tag < ActiveRecord::Base
   has_and_belongs_to_many :images
 
   validates_presence_of :name
-  
-  
+   
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -18,10 +18,13 @@ describe Image do
 
   tags = %w(tag-1 tag-2 tag-3 tag-4 tag-5 tag-6)
   tags_2 = %w(tag-7 tag-8 tag-9 tag-10)
+  
+  etypes = %w(People Places Groups Year Collection)
 
   before(:each) do
     Image.delete_all
     Tag.delete_all
+    EnumeratedType.delete_all
 
     # Alternate syntax for a create
     #@image = Image.new
@@ -40,13 +43,23 @@ describe Image do
       )
     end
 
-
+    etypes.each do |g|
+      EnumeratedType.create!(
+        :name => 'plugh',
+        :group_name => g
+      )
+    end
+    
   end
 
   it "should create tags " do
     Tag.all.count.should eq(tags.count)
   end
 
+  it "should create enumerated_types " do
+    EnumeratedType.all.count.should eq(etypes.count)
+  end
+  
   it "should add tags to a given image" do
     # Tag @image with all available tags, note an instance variable starting with an @
     # is different from a local variable
