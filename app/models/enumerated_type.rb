5fbad2ca8a7b2b1fe3ae14e9adaea592fa375a0b
hongloh <hongloh@gmail.com>
2012-04-17 20:54:25 -0400

    Sync with timjd resolving all conflicts

--- app/models/enumerated_type.rb
+++ app/models/enumerated_type.rb
@@ -1,13 +1,2 @@
-# == Schema Information
-#
-# Table name: enumerated_types
-#
-#  id         :integer(4)      not null, primary key
-#  group_name :string(255)
-#  name       :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class EnumeratedType < ActiveRecord::Base
 end
