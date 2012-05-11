dc9e95644c3bdb15daf433d713dbeb5a5c9d3623
hongloh <hongloh@gmail.com>
2012-05-11 01:01:20 -0400

    1. Fix test user creation 2. Remove commented out lines 3. Add basic test for 'My Info'

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -2,45 +2,44 @@ require 'spec_helper'
 
 describe "Homes", :type => :request do
   before :each do
-    User.create(:username => 'testuser', :password => 'good2012', :password_confirmation => 'hello', :email => 'test@email.com')
+    User.create(:username => 'testuser', :password => 'good2012', :password_confirmation => 'good2012', :email => 'test@email.com')
   end
   
-#  it 'should create a user with an encrypted password" do
-#    u = User.create(:username => 'testuser', :password => 'good2012', :password_confirmation => 'hello', :email => 'test@email.com')
-#    u.crypted_password.should_not eq('hello')
-#  end
-  
   describe "POST /homes" do
-    it "should register me, logout and login me again" do
+    it "should register me and logout" do
       visit homes_url
       click_link 'Register'
       page.should have_content("Username")
-      fill_in 'user[username]', :with => 'testuser'
-      fill_in 'user[password]', :with => 'good2012'
-      fill_in 'user[password_confirmation]', :with => 'good2012'
-      fill_in 'user[email]', :with => 'testuser@gmail.com'
+      fill_in 'user[username]', :with => 'testuserReg'
+      fill_in 'user[password]', :with => 'good2012Reg'
+      fill_in 'user[password_confirmation]', :with => 'good2012Reg'
+      fill_in 'user[email]', :with => 'testuserReg@gmail.com'
       click_button 'Submit'
       page.should have_content("Account registered!")
             
       click_link 'Log out'
-      # page.driver.browser.switch_to.alert.accept
-      # click_button 'OK'
-      page.should have_content("Successfully logged out")     
-        
+      page.should have_content("Successfully logged out")
+    end
+    
+    it "should log me in and check My Info" do
+      visit homes_url
       click_link 'Login'
       page.should have_content("Username")
       fill_in 'user_session[username]', :with => 'testuser'
       fill_in 'user_session[password]', :with => 'good2012'
       click_button 'Login'
       page.should have_content("Successfully logged in")
+      
+      click_link 'My Info'
+      page.should have_content("My Information")
     end
   
     it "should fail to log me in" do
       visit homes_url      
       click_link 'Login'
       page.should have_content("Username")
-      fill_in 'user_session[username]', :with => 'testuser'
-      fill_in 'user_session[password]', :with => 'good2012'
+      fill_in 'user_session[username]', :with => 'testuserBad'
+      fill_in 'user_session[password]', :with => 'good2012Bad'
       click_button 'Login'
       page.should have_content("Login failed")
     end
